ce6ae76b4ff49cbde94b07f83813555302c40384
toleenAA <toleenaa@vbox.home>
2025-05-24 16:17:45 -0400

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="legosec",
-    version="0.1.1",  # Start with a version number
+    version="0.1.1",  
     author="LegoSec Team",
     author_email="toleenabuadi@gmail.com",
     description="A security package for LEGO systems",
